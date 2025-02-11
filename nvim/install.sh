30d3913f06a5891c4e96344b6711ecad78d84ea4
Ryan White <ryan.s.white@jpl.nasa.gov>
2025-02-10 16:51:08 -0800

    Updated dotfiles on tirfsw-vm at Mon 10 Feb 2025 04:51:08 PM PST

--- nvim/install.sh
+++ nvim/install.sh
@@ -2,6 +2,8 @@
 
 INSTALL=$HOME/.local/nvim
 LOCALBIN=$HOME/.local/bin
+NVIMBUILD=nvim-linux-x86_64
+FILENAME=${NVIMBUILD}.tar.gz
 
 [ -d $INSTALL ] && rm -rf $INSTALL
 
@@ -10,11 +12,11 @@ mkdir -p $INSTALL
 cd $INSTALL
 
 # Download the binary
-curl -LO https://github.com/neovim/neovim/releases/latest/download/nvim-linux64.tar.gz
+curl -LO https://github.com/neovim/neovim/releases/latest/download/${FILENAME}
 
 # Extract the image
-tar -xzf nvim-linux64.tar.gz
+tar -xzf ${FILENAME}
 
 # Link the bin to user path
 [ ! -d $LOCALBIN ] && mkdir -p $LOCALBIN && echo Local bin may not be on path
-ln -s $INSTALL/nvim-linux64/bin/* $LOCALBIN/
+ln -s $INSTALL/${NVIMBUILD}/bin/* $LOCALBIN/
